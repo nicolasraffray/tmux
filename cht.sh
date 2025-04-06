87a8cce99c7962531c3a7b4314b0ff025f519b41
nicolasraffray <52550826+nicolasraffray@users.noreply.github.com>
2025-04-06 20:54:24 +0100

    initial commit

---
+++ cht.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+languages=`echo "csharp typescript python javascript" | tr ' ' '\n'`
+core_utils=`echo "xarge find mv sed awk" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read -p "query: " query
+
+if printf $languages | grep -qs $selected; then
+    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+fi
